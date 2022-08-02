d23952a9f6ce7cfca2db8b9a92af4a1d14fbdbfd
airandopal <airandopal@gmail.com>
2022-08-01 20:59:23 -0500

    chore: remove unused code

--- pages/main.js
+++ pages/main.js
@@ -6,29 +6,6 @@ import { imageData } from "@/data";
 import { ImageSection } from "@/components";
 import { useGlobalState } from "@/global-state";
 
-// done: check mobile view
-// done: clean css
-// done: separate out data
-// done: add loading
-// done: not right on deploy
-// done: add jsonconig for imports
-// todo: test pixel for pixel. on screenshots at different point
-// todo: is there a way to snapshot test animations?
-// todo: feature - add being able to add ones dynamically - unsplash, easy way vs tina cms?
-// todo: add sections with shapes, elements with shapes
-// todo: ooh interesting, mix up the photos between the layers
-// todo: can i put the bg urls in config?
-// zod?
-// maybe add some framer motion and reveals.
-// later: background iamge solution
-// later: why isn't the height being passed down to be able to use 100%
-// later: should i be using layout effect?
-// later: clean eslint
-// later: why doesnt background image work for tailwind - try it one more time.
-// later: jsdoc and typescript
-// later; working with aspect ratios
-
-// todo: 	<body class="loading cda-hidemobile">
 
 const Main = () => {
 	const loadingState = useGlobalState((state) => state.loadingState);
